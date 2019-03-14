cc38b1e51c515e63c9bbe7dc4d6fce928c7c95c1
alexmickelson96 <alexmickelson96@gmail.com>
2019-03-14 16:20:34 -0600

    trying to test ui

---
+++ UITest1/AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest1
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .EnableLocalScreenshots()
+                    .ApkFile("../../../Managment.Droid/bin/Release/Managment.Droid.Managment.Droid-Signed.apk")
+                    .StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
